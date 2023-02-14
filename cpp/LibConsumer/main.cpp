9671ea20ee00a58c1517a1c47ecc656ddd406701
nickolay-kondratyev <nickolay.kondratyev@gmail.com>
2023-02-13 22:24:21 -0800

    Adding C++ library build with CMake example, ref https://www.glassthought.com/notes/muuab6k26r20j848r6znup3/

---
+++ cpp/LibConsumer/main.cpp
@@ -0,0 +1,12 @@
+#include "../LibProducer/adder.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  // print hello world
+  cout << "1 + 2 = " << mymath::add(1,2) << endl;
+
+  return 0;
+}
---
+++ cpp/LibProducer/adder.cpp
@@ -0,0 +1,12 @@
+#include "adder.h"
+
+int mymath::add(int a, int b)
+{
+  return a + b;
+}
+
+float mymath::add(float a, float b)
+{
+  return a + b;
+}
+
