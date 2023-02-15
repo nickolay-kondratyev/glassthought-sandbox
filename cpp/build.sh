a95124bb07d15d49b9333676a5e17610a5db2f05
nickolay-kondratyev <nickolay.kondratyev@gmail.com>
2023-02-14 22:14:39 -0800

    Example add_subdirectory of git submodule ref: https://www.glassthought.com/notes/wyor2tmkhkdq2ws3yw2z2kh/

---
+++ cpp/build.sh
@@ -0,0 +1,1 @@
+cmake -S . -B ./build && cd build && make && echo "Build done."
---
+++ cpp/hi_world.cpp
@@ -0,0 +1,8 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hi World!\n";
+    return 0;
+}
