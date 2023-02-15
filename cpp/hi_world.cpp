c1fa9e00a76f22c15efe5b22b25c3efc9bcb6049
nickolay-kondratyev <nickolay.kondratyev@gmail.com>
2023-02-15 06:48:23 -0800

    using glfw, submodule library, ref: https://www.glassthought.com/notes/hy3rtizfktxdy1ipj1e8uhm/

--- cpp/hi_world.cpp
+++ cpp/hi_world.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
     std::cout << "Hi World!\n";
+
+    GLFWwindow* window;
+    int width, height;
+
+    if( !glfwInit() )
+    {
+      fprintf( stderr, "Failed to initialize GLFW\n" );
+      exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    while( !glfwWindowShouldClose(window) )
+    {
+
+        // Swap buffers
+        // glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    // Exit program
+    exit( EXIT_SUCCESS );
+
     return 0;
 }
