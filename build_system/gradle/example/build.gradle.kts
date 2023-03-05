b971e77517c9914c0350ca9ee3224086bc6dbe11
nickolay-kondratyev <nickolay.kondratyev@gmail.com>
2023-03-04 19:52:06 -0800

    Snapshot of command: 'gradle only-single-command-line-will-run' in ${GT_SANDBOX_REPO}/build_system/gradle/example
    ## Recorded output of command:
    ```
    
    > Configure project :
    configuration-printl1
    congiguration-printl2
    
    > Task :only-single-command-line-will-run
    doFirst-printl
    doFirst commandLine echo-1
    doLast printl-1
    doLast printl-2
    
    BUILD SUCCESSFUL in 590ms
    1 actionable task: 1 executed
    ```

--- build_system/gradle/example/build.gradle.kts
+++ build_system/gradle/example/build.gradle.kts
@@ -1,13 +1,26 @@
 task<Exec>("only-single-command-line-will-run"){
-    commandLine("echo", "outside commandLine echo-1")
-    commandLine("echo", "outside commandLine echo-2")
+    // These two commandLine are meaningless since they will be overwritten, within
+    // doFirst, by the commandLine in doFirst.
+    //
+    // The Exec task holds a single command line to run. commandLine does not
+    // run the command, it sets the command line variable on the Exec instance.
+    commandLine("echo", "configuration set echo-1")
+    commandLine("echo", "configuration set echo-2")
 
-    println("outside-printl1")
-    println("outside-printl2")
+    println("configuration-printl1")
+    println("congiguration-printl2")
+
+    doFirst {
+        println("doFirst-printl")
+
+        // This is the only commandLine that matters.
+        commandLine("echo", "doFirst commandLine echo-1")
+    }
 
     doLast {
+        // doLast is executed after the task action has executed, so setting the
+        // command line in here won't do anything useful.
         commandLine("echo", "doLast commandLine echo-1")
-        commandLine("echo", "doLast commandLine echo-2")
 
         println("doLast printl-1")
         println("doLast printl-2")
