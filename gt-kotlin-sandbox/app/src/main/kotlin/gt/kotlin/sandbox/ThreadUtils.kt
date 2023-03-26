fa87249afb8a655d191317da5148af4308fb301f
nickolay-kondratyev <nickolay.kondratyev@gmail.com>
2023-03-26 23:00:02 +0700

    SAVE

---
+++ gt-kotlin-sandbox/app/src/main/kotlin/gt/kotlin/sandbox/ThreadUtils.kt
@@ -0,0 +1,25 @@
+package gt.kotlin.sandbox
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ThreadUtils {
+    companion object {
+        private var lastTime: Long = System.currentTimeMillis()
+
+        fun printWithThreadInfo(msg: String) {
+            val thread = Thread.currentThread()
+            val threadName = thread.name
+            val threadId = thread.id
+            val timestamp = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Date())
+            val currentTime = System.currentTimeMillis()
+            val elapsedMillis = currentTime - lastTime
+            println("[$timestamp][$threadName-$threadId][${elapsedMillis}ms] $msg")
+            lastTime = currentTime
+        }
+
+        fun sleep(millis: Long){
+            Thread.sleep(millis)
+        }
+    }
+}
