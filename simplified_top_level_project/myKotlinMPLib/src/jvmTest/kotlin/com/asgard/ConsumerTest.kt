0693fde7115775ae423809c34a7e0f0c10d61768
Nickolay Kondratyev <nickolaykondratyev@Nickolays-MacBook-Pro.local>
2023-05-06 15:21:15 -0700

    Clarify tests

--- simplified_top_level_project/myKotlinMPLib/src/jvmTest/kotlin/com/asgard/ConsumerTest.kt
+++ simplified_top_level_project/myKotlinMPLib/src/jvmTest/kotlin/com/asgard/ConsumerTest.kt
@@ -5,13 +5,9 @@ import io.kotest.core.spec.style.DescribeSpec
 
 class ConsumerTest : DescribeSpec({
     describe("GIVEN starter describe block") {
-        describe("WHEN running tests") {
-            it("THEN 1+1 equals 2") {
-                assert(1 + 1 == 2)
-            }
-        }
 
-        it("WHEN calling start implementation THEN should respond with expected number") {
+
+        it("IMPORTANT WHEN calling consumer get number from dependency.") {
             val starter = ConsumerImplementationDependency()
 
             assert(starter.getInteger() == 42000)
