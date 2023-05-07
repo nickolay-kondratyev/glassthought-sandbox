ec634ec4afbea2086ef85d38b6e265e1204d953e
Nickolay Kondratyev <nickolaykondratyev@Nickolays-MacBook-Pro.local>
2023-05-06 22:16:10 -0700

    Adding gt-kotlin-sandbox back

---
+++ gt-kotlin-sandbox/app/src/main/kotlin/gt/kotlin/sandbox/App.kt
@@ -0,0 +1,42 @@
+package gt.kotlin.sandbox
+
+import kotlinx.coroutines.*
+import java.util.stream.Collectors.toList
+import java.util.stream.IntStream
+
+
+suspend fun performLongRequest(msg: String): String {
+    return withContext(Dispatchers.IO) {
+        ThreadUtils.printWithThreadInfo("Within subroutine (before sleep) input: $msg")
+
+        ThreadUtils.sleep(500)
+
+        String.format("MessageResultAfterBlockingOperation for [%s]", msg)
+    }
+}
+
+fun main() {
+
+    ThreadUtils.printWithThreadInfo("Example using async with more requests than cores on a laptop")
+
+    val mainMillisStamp = System.currentTimeMillis();
+
+
+    runBlocking {
+
+        val deferredObjects = IntStream.range(0, 8).mapToObj { i ->
+            val deferred = async { performLongRequest("${i}-request") }
+            deferred
+        }.collect(toList())
+
+        for(deferred in deferredObjects) {
+            ThreadUtils.printWithThreadInfo("Deferred result: " + deferred.await())
+        }
+
+        ThreadUtils.printWithThreadInfo(
+            "Total time taken: " +
+                    (System.currentTimeMillis() - mainMillisStamp) + "ms"
+        )
+    }
+
+}
