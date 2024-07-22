31fc35fa6085a9f8c372b8fb247199efea994182
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-07-21 17:57:56 -0700

    Snapshot of command: './run.sh' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    [33mNo-OP.(B[m
    [32mRun the app within Android Studio, refer to ./doc/how-to-run.md(B[m
    ```

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 main() {
   echo.yellow "No-OP."
-  echo.green "Run the app within Android Studio."
-  cat "./doc/how-to-run.md"
+  echo.green "Run the app within Android Studio, refer to ./doc/how-to-run.md"
 }
 
 main "${@}" || exit 1
