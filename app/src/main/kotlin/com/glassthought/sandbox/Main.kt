262033ba4386586229818fc987eb4bd0680ca73b
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-11-17 15:01:45 -0800

    Snapshot of command: './gradlew run --quiet' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -3,7 +3,9 @@ package com.glassthought.sandbox
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-  generateSequence(1) { it + 1 }
+  val seedValue = 1
+  
+  generateSequence(seedValue) { it + 1 }
     .take(10)
     .forEach { print("$it, ") }
 }
