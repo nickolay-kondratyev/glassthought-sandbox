8dd88ae2c1e754aa8590db7071bcc06e20022de7
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-11-21 20:45:40 -0800

    Snapshot of command: './gradlew run' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    > Task :app:checkKotlinGradlePluginConfigurationErrors SKIPPED
    > Task :app:processResources NO-SOURCE
    > Task :app:compileKotlin
    > Task :app:compileJava NO-SOURCE
    > Task :app:classes UP-TO-DATE
    
    > Task :app:run
    [2024-11-22T04:45:40.122087Z][elapsed-since-start:   39ms][tname:ForkJoinPool.commonPool-worker-1/tid:20] supplyAsync
    [2024-11-22T04:45:40.141102Z][elapsed-since-start:   49ms][tname:ForkJoinPool.commonPool-worker-1/tid:20] thenApply
    [2024-11-22T04:45:40.141452Z][elapsed-since-start:   49ms][tname:main/tid:1] thenApply().get(): Hello: Jon Snow
    [2024-11-22T04:45:40.141585Z][elapsed-since-start:   49ms][tname:main/tid:1] future.get(): [Jon Snow]
    
    BUILD SUCCESSFUL in 916ms
    2 actionable tasks: 2 executed
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -4,16 +4,19 @@ import gt.sandbox.util.output.Out
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.CompletableFuture
 
-val out = Out.standard();
+val out = Out.standard()
 
 fun main() = runBlocking {
   val future = CompletableFuture.supplyAsync {
-    out.println("Starting to sleep")
-    Thread.sleep(1000)
-    out.println("Finished sleeping")
-    "Hello, CompletableFuture!"
+    out.println("supplyAsync")
+    "Jon Snow"
   }
 
-  out.println("Hello, from Main thread")
-  out.println(future.get()) // Output: Hello, CompletableFuture!
+  out.println("thenApply().get(): " + future.thenApply {
+    out.println("thenApply")
+    "Hello: $it"
+  }.get())
+
+  out.println("""future.get(): [${future.get()}]""") // Output: Hello, CompletableFuture!
+  println()
 }
