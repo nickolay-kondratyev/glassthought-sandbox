1c4b9d2596b1f10289ca27970e138e997aa1d124
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-11-21 20:46:49 -0800

    Snapshot of command: './gradlew run' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    > Task :app:checkKotlinGradlePluginConfigurationErrors SKIPPED
    > Task :app:compileKotlin UP-TO-DATE
    > Task :app:compileJava NO-SOURCE
    > Task :app:processResources NO-SOURCE
    > Task :app:classes UP-TO-DATE
    
    > Task :app:run
    [2024-11-22T04:46:49.146281Z][elapsed-since-start:   43ms][tname:ForkJoinPool.commonPool-worker-1/tid:20] supplyAsync
    [2024-11-22T04:46:49.165936Z][elapsed-since-start:   52ms][tname:ForkJoinPool.commonPool-worker-1/tid:20] thenApply
    [2024-11-22T04:46:49.166359Z][elapsed-since-start:   53ms][tname:main/tid:1] thenApply().get(): Hello: Jon Snow
    [2024-11-22T04:46:49.166505Z][elapsed-since-start:   53ms][tname:main/tid:1] future.get(): [Jon Snow]
    
    BUILD SUCCESSFUL in 618ms
    2 actionable tasks: 1 executed, 1 up-to-date
    ```

---
+++ snapshot_gradle_run_with_main.sh
@@ -0,0 +1,6 @@
+main() {
+  export GT_SNAPSHOT_OPTIONAL_CODE_FILE_TO_INCLUDE_IN_MSG=${GT_SANDBOX_REPO:?}/app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+  gt.snapshot "./gradlew run"
+}
+
+main "${@}" || exit 1
