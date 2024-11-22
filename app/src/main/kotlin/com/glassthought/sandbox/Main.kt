7186d93f9f4e7f7d57421e640b91c02298906291
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-11-21 22:14:51 -0800

    Snapshot of command: './gradlew run' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    > Task :app:checkKotlinGradlePluginConfigurationErrors SKIPPED
    > Task :app:processResources NO-SOURCE
    > Task :app:compileKotlin
    > Task :app:compileJava NO-SOURCE
    > Task :app:classes UP-TO-DATE
    
    > Task :app:run
    [2024-11-22T06:14:51.019728Z][elapsed-since-start:   40ms][tname:main/tid:1] Caught exception: java.lang.RuntimeException: original-exc-msg-from-supplyAsync-future-block
    
    BUILD SUCCESSFUL in 764ms
    2 actionable tasks: 2 executed
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -10,26 +10,17 @@ val out = Out.standard()
 
 fun main() = runBlocking {
   val future = CompletableFuture.supplyAsync {
-    // WHEN I do NOT PRINT THIS LINE
-    //  WITHIN THE FEATURE
-    // THEN thenAccept runs on main thread.
-    // out.println("supplyAsync")
 
     throw RuntimeException("original-exc-msg-from-supplyAsync-future-block")
     "Jon Snow"
   }
 
-  future.handle { result: String, ex: Throwable? ->
-    if (ex != null) {
-      return@handle "Error handling: " + ex.message
-    } else {
-      return@handle result.uppercase(Locale.getDefault())
-    }
-  }.thenApply { x: String -> out.println("thenApply"); x + "!" }
-    .thenAccept { x: String -> out.println("thenAccept: " + x) }
+  try {
+    future.get()
+  } catch (e: Exception) {
+    out.println("Caught exception: ${e.message}")
+  }
 
-
-  delay(100)
-  println()
+  println("")
 }
 
