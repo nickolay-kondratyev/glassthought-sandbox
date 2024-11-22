b581f798c55d205c6e911649f9dbc6b0973252c5
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-11-21 20:28:04 -0800

    Snapshot of command: './gradlew run' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    > Task :app:checkKotlinGradlePluginConfigurationErrors SKIPPED
    > Task :app:processResources NO-SOURCE
    > Task :app:compileKotlin
    > Task :app:compileJava NO-SOURCE
    > Task :app:classes UP-TO-DATE
    
    > Task :app:run
    Some other work
    Result-Val
    
    BUILD SUCCESSFUL in 874ms
    2 actionable tasks: 2 executed
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -1,7 +1,15 @@
 package com.glassthought.sandbox
 
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.CompletableFuture
+
 
 fun main() = runBlocking {
-  println("Hello, World!")
+  val future = CompletableFuture<String>()
+  // Completing it programmatically
+  future.complete("Result-Val")
+
+  println("Some other work")
+
+  println(future.get()) // Output: Result-Val
 }
