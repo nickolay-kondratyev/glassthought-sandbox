f9282b62d211089b4a1669b1edbabddf17c4f8ff
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-11-28 14:47:26 -0800

    Snapshot of command: './gradlew run --quiet' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    [elapsed:   23ms][tname:main/tid:1][coroutine:unnamed] Before-1
    [elapsed:   46ms][tname:main/tid:1] SafeContinuation for Continuation at com.glassthought.sandbox.MainKt.main(Main.kt:12)
    [elapsed:   47ms][tname:main/tid:1] Before-2
    [elapsed:   47ms][tname:main/tid:1][coroutine:unnamed] After called because we resumed
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -1,6 +1,7 @@
 package com.glassthought.sandbox
 
 import gt.sandbox.util.output.Out
+import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 val out = Out.standard()
@@ -8,11 +9,16 @@ val out = Out.standard()
 suspend fun main() {
   out.info("Before-1")
 
-  suspendCoroutine<Unit> { continuation ->
+  suspendCoroutine<String>{ continuation ->
+    // This lambda function is called right before the suspension
+    // takes place.
     out.infoNonSuspend(continuation.toString())
 
     out.infoNonSuspend("Before-2")
+
+    // Question: where can we catch the result that we resumed with?
+    continuation.resume("Hello from suspended coroutine")
   }
 
-  out.info("After never called")
+  out.info("After called because we resumed")
 }
