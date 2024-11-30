7e4d307898ce704770e6e1242b26ce7fcb81420d
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-11-30 14:59:56 -0600

    Snapshot of command: './gradlew run --quiet' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    Expected Counter: 500000
    Actual Counter: 500000
    Difference between expected and actual: 0
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -4,15 +4,18 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
 import kotlin.system.measureTimeMillis
 
 // Shared counter variable
 private var counter = 0
 
+private val lock = Any()
+
 // Function that increments the counter inside a synchronized block
-private suspend fun incrementCounter(times: Int) {
+private fun incrementCounter(times: Int) {
   repeat(times) {
-    synchronized(counter) {
+    synchronized(lock) {
       counter++
     }
   }
@@ -23,17 +26,15 @@ fun main() = runBlocking {
   val coroutineCount = 5
   val incrementsPerCoroutine = 100_000 // Total increments = coroutineCount * incrementsPerCoroutine
 
-  val timeTaken = measureTimeMillis {
-    coroutineScope {
-      repeat(coroutineCount) {
-        launch(Dispatchers.Default) { incrementCounter(incrementsPerCoroutine) }
-      }
+  (1..coroutineCount).map {
+    thread {
+      incrementCounter(incrementsPerCoroutine)
     }
-  }
-  
+  }.forEach { it.join() }
+
+
   val expectedCounter = coroutineCount * incrementsPerCoroutine
   println("Expected Counter: $expectedCounter")
   println("Actual Counter: $counter")
   println("Difference between expected and actual: ${expectedCounter - counter}")
-  println("Time Taken: $timeTaken ms")
 }
