23ac2adb62f9c844078eaaf5b4fd70e6d65a56f5
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-12-13 18:54:08 -0800

    Snapshot of command: './gradlew run --quiet' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    [0] com.glassthought.sandbox.MainKt.callerTwo (Main.kt:29)
    [1] com.glassthought.sandbox.MainKt.callerOne (Main.kt:25)
    [2] com.glassthought.sandbox.MainKt.main (Main.kt:37)
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -6,23 +6,19 @@ import gt.sandbox.util.output.impl.OutSettings
 val out = Out.standard(outSettings = OutSettings(printColorPerThread = true))
 
 class StackTraceRetriever {
-
-
   companion object {
     /**
      * Returns the filtered stack trace starting from the first relevant caller.
      */
     fun getStackTrace(): List<StackTraceElement> {
-
       // Capture the stack trace and exclude this constructor and the wrapper class
       val allStackTrace = Throwable().stackTrace
+      
       return allStackTrace.dropWhile {
-        it.className == StackTraceRetriever::class.java.name
+        it.className == StackTraceRetriever::class.java.name || it.methodName == "getStackTrace"
       }
     }
   }
-
-
 }
 
 fun callerOne() {
