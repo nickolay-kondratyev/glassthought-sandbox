0281f04f0f049169c1b751f6e57019d1077f1512
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-12-14 19:23:25 -0800

    Snapshot of command: './gradlew run --quiet' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    [elapsed:   23ms][🥇/tname:main/tid:1][coroutine:unnamed] Hello, world!
    [elapsed:   45ms][⓶/tname:Thread-0/tid:20][coroutine:unnamed] Sleeping
    [elapsed: 1060ms][⓷/tname:kotlinx.coroutines.DefaultExecutor/tid:21][coroutine:unnamed] Goodbye, world!
    [elapsed: 1060ms][⓶/tname:Thread-0/tid:20][coroutine:unnamed] Done Sleeping
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -1,64 +1,43 @@
 package com.glassthought.sandbox
 
 import gt.sandbox.util.output.Out
-import com.glassthought.sandbox.util.out.impl.OutSettings
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlin.concurrent.thread
 
-val mutex = Mutex()
+val indexParsed = CompletableDeferred<Unit>()
+val directoriesWatched = CompletableDeferred<Unit>()
 
-var counter = 0
-
-suspend fun test() {
-  // mutex.withLock {
-    counter = counter + 1
-  // }
+suspend fun waitForAllStates() {
+  // Wait for both states to complete
+  indexParsed.await()
+  directoriesWatched.await()
 }
 
-val out = Out.standard(OutSettings(printCoroutineName = false))
-val TIMES_TO_REPEAT = 100000
+fun markIndexParsed() {
+  indexParsed.complete(Unit)
+}
+
+fun markDirectoriesWatched() {
+  directoriesWatched.complete(Unit)
+}
+
+val out = Out.standard()
 
 suspend fun main(args: Array<String>) {
-
-  out.info("Starting on main thread")
-
-  val t1 = thread {
+  out.info("Hello, world!")
+  thread {
     runBlocking {
-      incrementInALoop("thread-1")
+      out.info("Sleeping")
+      Thread.sleep(1000)
+      markIndexParsed()
+      markDirectoriesWatched()
+      out.info("Done Sleeping")
     }
   }
 
-  val t2 = thread {
-    runBlocking {
-      incrementInALoop("thread-2")
-    }
-  }
-
-  t1.join()
-  t2.join()
-
-  printResults()
-}
-
-private suspend fun incrementInALoop(threadName: String) {
-  out.info("Starting execution on $threadName")
-  repeat(TIMES_TO_REPEAT) {
-    test()
-  }
-  out.info("Finished execution on $threadName")
-}
-
-private suspend fun printResults() {
-  out.info("Counter : $counter")
-  val expected = TIMES_TO_REPEAT * 2
-  out.info("Expected: $expected")
-  if (expected == counter) {
-    out.printGreen("All accounted!")
-    out.println("")
-  } else {
-    out.printRed("NOT all accounted!")
-    out.println("")
-  }
+  waitForAllStates()
+  delay(1)
+  out.info("Goodbye, world!")
 }
