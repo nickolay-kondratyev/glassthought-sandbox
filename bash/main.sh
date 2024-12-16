e0b3223a8e1bb518336e8ea44cb443e6b3ac1e73
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-12-15 17:22:24 -0800

    Snapshot of command: './main.sh' in ${GT_SANDBOX_REPO}/bash
    ### Recorded output of command:
    ```
    Start in main- BASHPID=51453
    In parenthesis spawned subshell- BASHPID=51454
    End in main- BASHPID=51453
    ```

--- bash/main.sh
+++ bash/main.sh
@@ -1,5 +1,9 @@
 main() {
-  echo "echo-val"
+  echo "Start in main- BASHPID=$BASHPID"
+
+  (echo "In parenthesis spawned subshell- BASHPID=$BASHPID")
+
+  echo "End in main- BASHPID=$BASHPID"
 }
 
 main "${@}" || exit 1
