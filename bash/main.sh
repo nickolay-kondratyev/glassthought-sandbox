c4acfa9c4417db3671dc30848da98fe87a00095e
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-12-18 19:35:21 -0800

    Snapshot of command: './main.sh' in ${GT_SANDBOX_REPO}/bash
    ### Recorded output of command:
    ```
    
    Running Sample Command that fails (returns 1)...
    Pipe status captured as expected: 1
    
    Running Sample Command that fails (returns 1)...
    Pipe status got reset without a PIPE call: 0
    ```

--- bash/main.sh
+++ bash/main.sh
@@ -1,9 +1,28 @@
+# Define a sample function for testing the behavior of PIPESTATUS
+sample_command_that_fails() {
+  echo ""
+  echo 'Running Sample Command that fails (returns 1)...'
+  return 1
+}
+
+# Temporary file for output capture
+OUTPUT_FILE="/tmp/sample_output.txt"
+
+correct(){
+  sample_command_that_fails | tee "${OUTPUT_FILE:?}"
+  echo.green "Pipe status captured as expected: ${PIPESTATUS[0]}"
+}
+
+
+incorrect(){
+    sample_command_that_fails | tee "${OUTPUT_FILE:?}"
+    SOME_VAR="${OUTPUT_FILE:?}"
+    echo.yellow "Pipe status got reset without a PIPE call: ${PIPESTATUS[0]}"
+}
+
 main() {
-  echo "Start in main- BASHPID=$BASHPID"
-
-  (echo "In parenthesis spawned subshell- BASHPID=$BASHPID")
-
-  echo "End in main- BASHPID=$BASHPID"
+  correct
+  incorrect
 }
 
 main "${@}" || exit 1
