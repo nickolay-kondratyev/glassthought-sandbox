82bcecd6e964af20b0ba9741643d8440a4d39639
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-12-23 19:50:11 -0800

    Snapshot of command: './gradlew run --quiet' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    [elapsed:  305ms][🥇/tname:main/tid:1][coroutine:unnamed] Expected value: 20000000 actual value: 20000000.
    [elapsed:  316ms][🥇/tname:main/tid:1][coroutine:unnamed] Counter is equal to expected value
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -2,25 +2,26 @@ package com.glassthought.sandbox
 
 import gt.sandbox.util.output.Out
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
 
 fun main() = runBlocking {
   val out = Out.standard() // Assuming Out.standard() is implemented elsewhere
 
   // Shared variable
-  var counter = 0
+  val counter = AtomicInteger(0)
   val iterations = 10000000
 
   // Threads
   val thread1 = thread {
     repeat(iterations) {
-      counter++
+      counter.incrementAndGet()
     }
   }
 
   val thread2 = thread {
     repeat(iterations) {
-      counter++
+      counter.incrementAndGet()
     }
   }
 
@@ -30,9 +31,10 @@ fun main() = runBlocking {
 
   // Display results
   val expected = iterations * 2
-  out.infoGreen("Expected value: $expected actual value: $counter.")
-  if (counter != expected) {
-    out.infoRed("Counter is not equal to expected value, difference: ${expected - counter}")
+  val finalValue = counter.get()
+  out.infoGreen("Expected value: $expected actual value: $finalValue.")
+  if (finalValue != expected) {
+    out.infoRed("Counter is not equal to expected value, difference: ${expected - finalValue}")
   } else {
     out.infoGreen("Counter is equal to expected value")
   }
