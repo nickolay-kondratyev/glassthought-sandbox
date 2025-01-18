30ba6e57729651221b78944366b7ec09974a049b
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-01-18 06:43:28 -0800

    Snapshot of command: './gradlew run --quiet' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    === Benchmark Results for Thread.currentThread().name ===
    Executed 1000000 times
    Total time:  17.621 milliseconds
    Average:     17 ns
    Min:         0 ns
    Max:         22.583 microseconds
    P50:         0 ns  ((â runs/ms))
    P90:         42 ns  ((23809.524 runs/ms))
    P99:         84 ns  ((11904.762 runs/ms))
    P99.9:       291 ns  ((3436.426 runs/ms))
    P100:        22.583 microseconds  ((44.281 runs/ms))
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -1,61 +1,20 @@
 package com.glassthought.sandbox
 
-import kotlinx.coroutines.*
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import gt.sandbox.util.output.out
+import com.glassthought.sandbox.util.benchmarker.Benchmarker
+import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
 
-fun main() = runBlocking {
-  val mutex = Mutex()
-  val expectedOrder = listOf("Job 1", "Job 2", "Job 3")
-  val results = mutableListOf<String>()
+fun main(args: Array<String>) {
+  thread {
+    runBlocking {
+      val result = Benchmarker.benchmark(
+        {
+          Thread.currentThread().name
+        },
+        name = "Thread.currentThread().name",
+      )
 
-  var counterTimesRan = 0
-  while (true) {
-    results.clear()
-
-   val mutexAcquired = CompletableDeferred<Unit>()
-    val job1 = launch {
-      out.info("Job 1: Attempting to acquire the mutex...")
-      mutex.withLock {
-        out.infoGreen("Job 1: Acquired the mutex!")
-        mutexAcquired.complete(Unit)
-        results.add("Job 1")
-        delay(200) // Simulate long work
-        out.infoGreen("Job 1: Releasing the mutex.")
-      }
-    }
-    mutexAcquired.await()
-
-    val job2AboutToWaitOnMutex = CompletableDeferred<Unit>()
-    val job2 = launch {
-      out.info("Job 2: Attempting to acquire the mutex...")
-      job2AboutToWaitOnMutex.complete(Unit)
-      mutex.withLock {
-        out.infoBlue("Job 2: Acquired the mutex!")
-        results.add("Job 2")
-        out.infoBlue("Job 2: Releasing the mutex.")
-      }
-    }
-    job2AboutToWaitOnMutex.await()
-    delay(10)
-
-    val job3 = launch {
-      out.info("Job 3: Attempting to acquire the mutex...")
-      mutex.withLock {
-        out.infoRed("Job 3: Acquired the mutex!")
-        results.add("Job 3")
-        out.infoRed("Job 3: Releasing the mutex.")
-      }
-    }
-
-    joinAll(job1, job2, job3)
-
-    if (results != expectedOrder) {
-      out.infoRed("Incorrect order detected: $results")
-      break
-    } else {
-      out.infoGreen("Correct order: $results, (run #${++counterTimesRan})")
+      println(result)
     }
   }
 }
