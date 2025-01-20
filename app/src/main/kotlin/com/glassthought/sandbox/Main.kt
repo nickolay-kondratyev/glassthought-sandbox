f4704d34f92de48ad25de3cc351499c0df32e5b4
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-01-20 10:39:56 -0800

    Snapshot of command: './gradlew run --quiet' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    [elapsed:   50ms][🥇/tname:main/tid:1][coroutine:producer] Sending for: 1
    [elapsed:  570ms][🥇/tname:main/tid:1][coroutine:producer] Sending for: 2
    [elapsed:  571ms][🥇/tname:main/tid:1][coroutine:unnamed] 1
    [elapsed: 1073ms][🥇/tname:main/tid:1][coroutine:producer] Sending for: 3
    [elapsed: 1073ms][🥇/tname:main/tid:1][coroutine:unnamed] 4
    [elapsed: 1575ms][🥇/tname:main/tid:1][coroutine:producer] Sending for: 4
    [elapsed: 1576ms][🥇/tname:main/tid:1][coroutine:unnamed] 9
    [elapsed: 2081ms][🥇/tname:main/tid:1][coroutine:producer] Sending for: 5
    [elapsed: 2081ms][🥇/tname:main/tid:1][coroutine:unnamed] 16
    [elapsed: 2589ms][🥇/tname:main/tid:1][coroutine:unnamed] 25
    Done!
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -1,20 +1,33 @@
 package com.glassthought.sandbox
 
 import com.glassthought.sandbox.util.benchmarker.Benchmarker
+import com.glassthought.sandbox.util.out.impl.OutSettings
+import gt.sandbox.util.output.Out
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
-fun main(args: Array<String>) {
-  thread {
-    runBlocking {
-      val result = Benchmarker.benchmark(
-        {
-          Thread.currentThread().name
-        },
-        name = "Thread.currentThread().name",
-      )
+val out = Out.standard(OutSettings(printCoroutineName = true))
 
-      println(result)
+fun main(args: Array<String>) {
+  runBlocking {
+    val channel = Channel<Int>()
+    launch(context = CoroutineName("producer")) {
+      // this might be heavy CPU-consuming computation or async logic,
+      // we'll just send five squares
+      for (x in 1..5) {
+        out.info("Sending for: $x")
+        delay(500)
+        channel.send(x * x)
+      }
     }
+
+    // here we print five received integers:
+    repeat(5) { out.info(channel.receive().toString()) }
+    println("Done!")
   }
+
 }
