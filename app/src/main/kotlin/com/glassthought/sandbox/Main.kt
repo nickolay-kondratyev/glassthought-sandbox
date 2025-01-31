bed37c930bb1de4223cb06c30a4c47092df7a6c7
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-01-31 15:07:24 -0800

    Snapshot of command: './gradlew run --quiet' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    [elapsed:   51ms][🥇/tname:main/tid:1][coroutine:unnamed] --------------------------------------------------------------------------------
    [elapsed:   62ms][🥇/tname:main/tid:1][coroutine:unnamed] Example where the server is aborted prior to finishing:
    [elapsed:   65ms][🥇/tname:main/tid:1][coroutine:unnamed] Starting server
    [elapsed:   73ms][⓶/tname:DefaultDispatcher-worker-1/tid:20][coroutine:ServerWork-1] Running server work in thread: DefaultDispatcher-worker-1
    [elapsed:   74ms][⓷/tname:DefaultDispatcher-worker-2/tid:21][coroutine:ServerWork-2] Running additional server work in thread: DefaultDispatcher-worker-2
    [elapsed: 1084ms][🥇/tname:main/tid:1][coroutine:unnamed] Stopping server
    [elapsed: 1087ms][🥇/tname:main/tid:1][coroutine:unnamed] --------------------------------------------------------------------------------
    [elapsed: 1087ms][🥇/tname:main/tid:1][coroutine:unnamed] Example where the server has time to finish:
    [elapsed: 1087ms][🥇/tname:main/tid:1][coroutine:unnamed] Starting server
    [elapsed: 1088ms][⓶/tname:DefaultDispatcher-worker-1/tid:20][coroutine:ServerWork-1] Running server work in thread: DefaultDispatcher-worker-1
    [elapsed: 1088ms][⓷/tname:DefaultDispatcher-worker-2/tid:21][coroutine:ServerWork-2] Running additional server work in thread: DefaultDispatcher-worker-2
    [elapsed: 2594ms][⓷/tname:DefaultDispatcher-worker-2/tid:21][coroutine:ServerWork-2] ServerWork-2  completed
    [elapsed: 3093ms][⓷/tname:DefaultDispatcher-worker-2/tid:21][coroutine:ServerWork-1] ServerWork-1 completed
    [elapsed: 4089ms][🥇/tname:main/tid:1][coroutine:unnamed] Stopping server
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -1,63 +1,58 @@
 package com.glassthought.sandbox
 
-import com.glassthought.sandbox.util.out.impl.OutSettings
 import gt.sandbox.util.output.Out
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
-val out = Out.standard(OutSettings(printCoroutineName = true, printThreadInfo = false))
+interface Server {
+  suspend fun start()
+  suspend fun stop()
+}
 
-fun main(args: Array<String>) {
-  runBlocking {
-    val channel = Channel<Int>(Channel.UNLIMITED)
+val out = Out.standard()
 
-    val sender = launch(CoroutineName("${Emoji.LETTER}-sender")) {
-      try {
-        repeat(1) {
-          delay(500)
+class ServerImpl(
+  private val scope: CoroutineScope // Injected scope for better control
+) : Server {
 
-          out.info("starting_to_send: $it")
+  private val job = SupervisorJob() // Ensures independent coroutines
+  private val serverScope = scope + job
 
-          channel.send(it)
+  override suspend fun start() {
+    out.info("Starting server")
 
-          out.info("sent: $it")
-        }
-      } catch (e: Exception) {
-        out.infoRed("sender exception: $e")
-      }
+    serverScope.launch(CoroutineName("ServerWork-1")) {
+      out.info("Running server work in thread: ${Thread.currentThread().name}")
+      delay(2000)
+      out.info("ServerWork-1 completed")
     }
 
-    val listener = launch(CoroutineName("${Emoji.MAILBOX}-listener")) {
-      try {
-        for (i in channel) {
-          out.info("received: $i")
-        }
-        out.info("Listener Done - ${Emoji.CHECKERED_FLAG}")
-      } catch (e: Exception) {
-        out.infoRed("listener exception: $e")
-      }
+    serverScope.launch(CoroutineName("ServerWork-2")) {
+      out.info("Running additional server work in thread: ${Thread.currentThread().name}")
+      delay(1500)
+      out.info("ServerWork-2  completed")
     }
+  }
 
-    delay(1700)
-    out.info("listener.isActive: " + listener.isActive.toString())
-    out.info("Closing channel from Main")
-    channel.close()
-    delay(100)
-    out.info("listener.isActive: " + listener.isActive.toString())
+  override suspend fun stop() {
+    out.info("Stopping server")
 
-    delay(2000)
-    out.info("Main completed. Exiting...")
-    System.exit(0)
+    job.cancel()
   }
 }
 
-class Emoji {
-  companion object {
-    const val LETTER = "✉\uFE0F"
-    const val MAILBOX = "\uD83D\uDCEC"
-    const val CHECKERED_FLAG = "\uD83C\uDFC1"
-  }
+fun main() = runBlocking {
+  out.info("--------------------------------------------------------------------------------")
+  out.info("Example where the server is aborted prior to finishing:")
+  runWithDelayBeforeStopping(1000)
+
+  out.info("--------------------------------------------------------------------------------")
+  out.info("Example where the server has time to finish:")
+  runWithDelayBeforeStopping(3000)
+}
+
+private suspend fun runWithDelayBeforeStopping(delayBeforeStopping: Long) {
+  val server = ServerImpl(CoroutineScope(Dispatchers.Default)) // Inject external scope
+  server.start()
+  delay(delayBeforeStopping) // Use delay instead of Thread.sleep
+  server.stop()
 }
