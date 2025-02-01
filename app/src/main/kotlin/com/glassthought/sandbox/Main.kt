7511160be3eb888a6206c988f47f82943fa6923c
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-01-31 16:49:52 -0800

    Snapshot of command: './gradlew run --quiet' in ${GT_SANDBOX_REPO}
    ### Recorded output of command:
    ```
    [elapsed:   40ms][🥇/tname:main/tid:1][coroutine:unnamed] Starting server
    [elapsed:   58ms][🥇/tname:main/tid:1][coroutine:unnamed] Starting server
    [elapsed:   58ms][🥇/tname:main/tid:1][coroutine:unnamed] Stopping server: server-1
    [elapsed:   58ms][⓷/tname:DefaultDispatcher-worker-2/tid:21][coroutine:server-2-work-1] Running server work in thread: DefaultDispatcher-worker-2
    [elapsed:   58ms][⓶/tname:DefaultDispatcher-worker-1/tid:20][coroutine:server-1-work-1] Running server work in thread: DefaultDispatcher-worker-1
    [elapsed: 2063ms][⓶/tname:DefaultDispatcher-worker-1/tid:20][coroutine:server-2-work-1] server-2-work-1 completed
    ```

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -6,53 +6,49 @@ import kotlinx.coroutines.*
 interface Server {
   suspend fun start()
   suspend fun stop()
+  suspend fun join()
 }
 
 val out = Out.standard()
 
 class ServerImpl(
+  private val serverName: String,
   private val scope: CoroutineScope // Injected scope for better control
 ) : Server {
-
-  private val job = SupervisorJob() // Ensures independent coroutines
-  private val serverScope = scope + job
+  private var backgroundJob: Job? = null
 
   override suspend fun start() {
     out.info("Starting server")
 
-    serverScope.launch(CoroutineName("ServerWork-1")) {
+    backgroundJob = scope.launch(CoroutineName("${serverName}-work-1")) {
       out.info("Running server work in thread: ${Thread.currentThread().name}")
       delay(2000)
-      out.info("ServerWork-1 completed")
-    }
-
-    serverScope.launch(CoroutineName("ServerWork-2")) {
-      out.info("Running additional server work in thread: ${Thread.currentThread().name}")
-      delay(1500)
-      out.info("ServerWork-2  completed")
+      out.info("${serverName}-work-1 completed")
     }
   }
 
   override suspend fun stop() {
-    out.info("Stopping server")
+    out.info("Stopping server: $serverName")
 
-    job.cancel()
+    scope.cancel()
+  }
+
+  override suspend fun join() {
+    if (backgroundJob != null) {
+      backgroundJob!!.join()
+    }
   }
 }
 
 fun main() = runBlocking {
-  out.info("--------------------------------------------------------------------------------")
-  out.info("Example where the server is aborted prior to finishing:")
-  runWithDelayBeforeStopping(1000)
+  val server1 = ServerImpl("server-1", CoroutineScope(Dispatchers.Default))
+  val server2 = ServerImpl("server-2", CoroutineScope(Dispatchers.Default))
 
-  out.info("--------------------------------------------------------------------------------")
-  out.info("Example where the server has time to finish:")
-  runWithDelayBeforeStopping(3000)
-}
+  server1.start()
+  server2.start()
 
-private suspend fun runWithDelayBeforeStopping(delayBeforeStopping: Long) {
-  val server = ServerImpl(CoroutineScope(Dispatchers.Default)) // Inject external scope
-  server.start()
-  delay(delayBeforeStopping) // Use delay instead of Thread.sleep
-  server.stop()
+  server1.stop()
+
+  server1.join()
+  server2.join()
 }
