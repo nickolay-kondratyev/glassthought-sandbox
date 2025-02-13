e47a41c688e428a3497e73e6f6a327f091ba2597
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-02-13 15:41:11 -0800

    [2025-02-13T15-41-10PST] Modified files: Main.kt

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -1,2 +1,29 @@
 package com.glassthought.sandbox
 
+import gt.sandbox.util.output.out
+
+fun main() {
+    var counter = 0
+    val numberOfThreads = 3
+    val incrementsPerThread = 1000
+
+    val threads = List(numberOfThreads) { threadNum ->
+        Thread {
+            for (i in 1..incrementsPerThread) {
+                val current = counter
+                // Simulate some processing time to increase chance of race condition
+                Thread.sleep(1)
+                counter = current + 1
+                out.println("Thread $threadNum: counter = $counter")
+            }
+        }
+    }
+
+    out.printlnGreen("Starting threads...")
+    threads.forEach { it.start() }
+    threads.forEach { it.join() }
+
+    out.printlnRed("\nFinal counter value: $counter")
+    out.printlnBlue("Expected value: ${numberOfThreads * incrementsPerThread}")
+}
+
