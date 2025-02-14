13f527241e0b6a18d094808e938467720ea6231c
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-02-14 11:33:32 -0800

    [2025-02-14T11-33-32PST] Modified files: cursor_rules_location.mdc workspace.xml Main.kt

--- app/src/main/kotlin/com/glassthought/sandbox/Main.kt
+++ app/src/main/kotlin/com/glassthought/sandbox/Main.kt
@@ -1,2 +1,28 @@
 package com.glassthought.sandbox
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import gt.sandbox.util.output.Out
+
+fun main() = runBlocking {
+    val channel = Channel<String>()
+    val out = Out.standard()
+
+    // Launch a coroutine to send messages
+    launch {
+        val messages = listOf("Hello", "from", "Kotlin", "Channels")
+        for (msg in messages) {
+            channel.send(msg)
+            delay(500) // Simulate some work
+        }
+        channel.close() // Close the channel when done
+    }
+
+    // Launch a coroutine to receive messages
+    launch {
+        for (msg in channel) {
+            out.println(msg) // Use Out interface for output
+        }
+    }
+}
+
