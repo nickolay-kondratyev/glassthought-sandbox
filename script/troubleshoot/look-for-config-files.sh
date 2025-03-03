f696131c196407cf4174f4eedcd2fe5cd089af6b
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-03-03 15:44:49 -0800

    [2025-03-03T15-44-49PST] Added files: look-for-config-files.sh

---
+++ script/troubleshoot/look-for-config-files.sh
@@ -0,0 +1,17 @@
+# Search for job configuration files that mention 'main' in docker.
+echo "Searching for job configuration files mentioning 'main'..."
+
+docker exec -it thorg-jenkins bash -c '
+    # Look in jobs directory
+    echo "=== Job Configuration Files ==="
+    find /var/jenkins_home/jobs -type f -name "config.xml" -exec grep -l "main" {} \; | while read file; do
+        echo "File: $file"
+        grep -n "main" "$file" | head -10
+        echo ""
+    done
+
+    # Count total occurrences
+    echo "Total occurrences of \"main\" in job configurations:"
+    find /var/jenkins_home/jobs -type f -name "config.xml" -exec grep -l "main" {} \; | wc -l
+    echo ""
+'
