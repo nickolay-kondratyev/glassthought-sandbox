2c825b302c6c557701ee40028c6a3475188da0fb
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-03-04 10:47:04 -0800

    [2025-03-04T10-47-04PST] Modified files: _check_plugins.sh

--- _check_plugins.sh
+++ _check_plugins.sh
@@ -2,9 +2,11 @@ include "./_env_setup_source_me.sh"
 
 
 main() {
-  echo "With CURL:"
+  echo "Checking plugins with CURL:"
   curl -u admin:"$(./_password.sh)" -s http://localhost:8080/pluginManager/api/json
 
+  echo ""
+  echo ""
   # shellcheck disable=SC2012
   echo "There are [$(ls "${JENKINS_HOME:?}"/plugins | wc -l)] in [${JENKINS_HOME:?}/plugins] directory"
 }
