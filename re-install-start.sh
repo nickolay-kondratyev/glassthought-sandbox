6ae6a7e7365454ebb8d9a0ea90c6541f2ec35931
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-03-04 11:34:57 -0800

    [2025-03-04T11-34-57PST] Modified files: re-install-start.sh

--- re-install-start.sh
+++ re-install-start.sh
@@ -2,7 +2,10 @@ source "_env_setup_source_me.sh"
 main() {
   ./uninstall-jenkins.sh || throw "Failed to uninstall Jenkins"
   ./install-jenkins.sh || throw "Failed to install Jenkins"
-  sudo ./_configure-jenkins-home.sh || throw "Failed to configure Jenkins home"
+
+  # run configure-jenkins-home.sh with sudo in a subshell
+  (sudo ./_configure-jenkins-home.sh || throw "Failed to configure Jenkins home")
+
   ./start-jenkins.sh || throw "Failed to start Jenkins"
   ./install-plugins.sh || throw "Failed to install plugins"
   ./stop-jenkins.sh || throw "Failed to stop Jenkins"
