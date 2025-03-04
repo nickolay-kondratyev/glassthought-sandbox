9d9aed6a95b82f3cf27c92f3d3136c3b97c90586
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-03-04 10:45:28 -0800

    [2025-03-04T10-45-28PST] num_new_files=1, num_renamed_files=1, num_deleted_files=1

---
+++ _check_plugins.sh
@@ -0,0 +1,12 @@
+include "/_env_setup_source_me.sh"
+
+
+main() {
+  echo "With CURL:"
+  curl -u admin:"$(./_password.sh)" -s http://localhost:8080/pluginManager/api/json
+
+  echo "With ls:"
+  ls -la "${JENKINS_HOME:?}"/plugins
+}
+
+main "${@}" || exit 1
--- _password.sh
+++ _password.sh
@@ -1,3 +1,3 @@
-source "../_env_setup_source_me.sh"
+include "/_env_setup_source_me.sh"
 
 cat "${JENKINS_HOME:?}/secrets/initialAdminPassword"
