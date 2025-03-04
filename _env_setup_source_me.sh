14e6c7024da7deb602d4c3b8ece6d663dd3a3e83
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-03-04 10:54:24 -0800

    [2025-03-04T10-54-24PST] Modified files: _env_setup_source_me.sh install-plugins.sh

--- _env_setup_source_me.sh
+++ _env_setup_source_me.sh
@@ -6,10 +6,6 @@ export JENKINS_CLI_JAR="${JENKINS_HOME:?}/jenkins-cli.jar"
 export JENKINS_URL="http://localhost:8080"
 export ADMIN_USER="admin"
 
-# Note this might not actually take effect right now and contents of
-# ${JENKINS_HOME:?}/secrets/initialAdminPassword file need to be read instead.
-export ADMIN_PASSWORD="admin"
-
 # Colors for output
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
--- install-plugins.sh
+++ install-plugins.sh
@@ -4,22 +4,31 @@ set -e
 
 source "_env_setup_source_me.sh"
 
-JENKINS_URL=http://localhost:8080
-ADMIN_PASSWORD=$(./_password.sh)
+export ADMIN_PASSWORD=$(./_password.sh)
+
+_install_plugin(){
+  local plugin="${1:?plugin}"
+
+  java -jar "${JENKINS_CLI_JAR:?}" -s "${JENKINS_URL:?}" \
+    -auth admin:"${ADMIN_PASSWORD:?}" \
+    install-plugin "${plugin:?}"
+}
+
 
 echo.log "Password: ${ADMIN_PASSWORD:?}"
 
 echo.log "Installing plugins using Jenkins CLI"
 
+
+
 cat plugins.txt | while IFS= read -r plugin; do
   echo.log "Installing plugin: $plugin"
-  java -jar "${JENKINS_CLI_JAR:?}" -s $JENKINS_URL \
-    -auth admin:"$ADMIN_PASSWORD" \
-    install-plugin "$plugin"
+
+  _install_plugin "$plugin"
 done
 echo.log "Finished installing plugins"
 
 echo.log "Restarting Jenkins"
 java -jar "${JENKINS_CLI_JAR:?}" -s $JENKINS_URL \
-  -auth admin:"$ADMIN_PASSWORD" \
+  -auth admin:"${ADMIN_PASSWORD:?}" \
   safe-restart
