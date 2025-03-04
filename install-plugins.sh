99cf742e2649a2e44bee5c463993aba84c5ad0ba
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-03-04 10:52:00 -0800

    [2025-03-04T10-52-00PST] Modified files: install-plugins.sh

--- install-plugins.sh
+++ install-plugins.sh
@@ -10,12 +10,13 @@ ADMIN_PASSWORD=$(./_password.sh)
 echo.log "Password: ${ADMIN_PASSWORD:?}"
 
 echo.log "Installing plugins using Jenkins CLI"
-while read -r plugin; do
+
+cat plugins.txt | while IFS= read -r plugin; do
   echo.log "Installing plugin: $plugin"
   java -jar "${JENKINS_CLI_JAR:?}" -s $JENKINS_URL \
     -auth admin:"$ADMIN_PASSWORD" \
     install-plugin "$plugin"
-done < plugins.txt
+done
 echo.log "Finished installing plugins"
 
 echo.log "Restarting Jenkins"
