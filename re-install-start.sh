6412148a260d5a7b6720b79c91e538e65dbd4a30
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-03-04 10:44:42 -0800

    [2025-03-04T10-44-42PST] Modified files: re-install-start.sh check_plugins.sh

--- re-install-start.sh
+++ re-install-start.sh
@@ -5,6 +5,8 @@ main() {
   sudo ./_configure-jenkins-home.sh || throw "Failed to configure Jenkins home"
   ./start-jenkins.sh || throw "Failed to start Jenkins"
   ./install-plugins.sh || throw "Failed to install plugins"
+  ./stop-jenkins.sh || throw "Failed to stop Jenkins"
+  ./start-jenkins.sh || throw "Failed to start Jenkins"
 }
 
 main "${@}" || exit 1
--- troubleshoot/check_plugins.sh
+++ troubleshoot/check_plugins.sh
@@ -1,5 +1,12 @@
+source "../_env_setup_source_me.sh"
+
+
 main() {
+  echo "With CURL:"
   curl -u admin:"$(./password.sh)" -s http://localhost:8080/pluginManager/api/json
+
+  echo "With ls:"
+  ls -la "${JENKINS_HOME:?}"/plugins
 }
 
 main "${@}" || exit 1
