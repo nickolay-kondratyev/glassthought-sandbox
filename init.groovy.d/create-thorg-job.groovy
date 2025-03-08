ae4fe62fadaf976fe5538042df8571fdd330c950
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-03-08 08:37:39 -0800

    [2025-03-08T08-37-39PST] Modified files: create-thorg-job.groovy

--- init.groovy.d/create-thorg-job.groovy
+++ init.groovy.d/create-thorg-job.groovy
@@ -1,17 +1,11 @@
-import jenkins.model.Jenkins
-import org.jenkinsci.plugins.workflow.job.WorkflowJob
-import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
-import org.jenkinsci.plugins.workflow.job.WorkflowJob
-import hudson.triggers.SCMTrigger
+import com.cloudbees.hudson.plugins.folder.*
+import hudson.plugins.git.BranchSpec
 import hudson.plugins.git.GitSCM
 import hudson.plugins.git.UserRemoteConfig
-import hudson.plugins.git.BranchSpec
-import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
-import com.cloudbees.hudson.plugins.folder.*
-import hudson.model.Item
-import hudson.triggers.Trigger
-import hudson.triggers.TriggerDescriptor
-
+import hudson.triggers.SCMTrigger
+import jenkins.model.Jenkins
+import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
+import org.jenkinsci.plugins.workflow.job.WorkflowJob
 // Get Jenkins instance
 def jenkins = Jenkins.getInstance()
 
@@ -45,10 +39,10 @@ if (job == null) {
     // Configure SCM polling with optimized settings
     SCMTrigger trigger = new SCMTrigger('* * * * *')
     trigger.setIgnorePostCommitHooks(false)
-    
+
     // Clear any existing triggers first
     job.getTriggers().clear()
-    
+
     // Add the new trigger
     job.addTrigger(trigger)
 
@@ -75,11 +69,11 @@ if (job == null) {
 
     // Remove all existing triggers
     job.getTriggers().clear()
-    
+
     // Configure SCM polling with optimized settings
     SCMTrigger trigger = new SCMTrigger('* * * * *')
     trigger.setIgnorePostCommitHooks(false)
-    
+
     // Add the new trigger
     job.addTrigger(trigger)
 
