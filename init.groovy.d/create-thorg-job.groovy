89eae4b8152ae1603094d19f5793f533f0ee6259
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2025-03-08 08:43:34 -0800

    Making more dry

--- init.groovy.d/create-thorg-job.groovy
+++ init.groovy.d/create-thorg-job.groovy
@@ -6,6 +6,48 @@ import hudson.triggers.SCMTrigger
 import jenkins.model.Jenkins
 import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
 import org.jenkinsci.plugins.workflow.job.WorkflowJob
+
+// Function to create Git SCM configuration
+def createGitScm() {
+    return new GitSCM(
+        [new UserRemoteConfig('git@gitlab.com:thorg/thorg-root.git', null, null, 'gitlab-ssh-key')],
+        [new BranchSpec('*/master-sandbox')],
+        false,
+        [],
+        null,
+        null,
+        []
+    )
+}
+
+// Function to configure SCM polling trigger
+def configureSCMTrigger(job) {
+    // Clear any existing triggers first
+    job.getTriggers().clear()
+    
+    // Configure SCM polling with optimized settings
+    SCMTrigger trigger = new SCMTrigger('* * * * *')
+    trigger.setIgnorePostCommitHooks(false)
+    
+    // Add the new trigger
+    job.addTrigger(trigger)
+}
+
+// Function to configure job with pipeline definition and SCM trigger
+def configureJob(job) {
+    // Define Git SCM
+    def gitScm = createGitScm()
+    
+    // Set the pipeline definition using SCM
+    job.setDefinition(new CpsScmFlowDefinition(gitScm, 'Jenkinsfile'))
+    
+    // Configure SCM polling
+    configureSCMTrigger(job)
+    
+    // Save the job
+    job.save()
+}
+
 // Get Jenkins instance
 def jenkins = Jenkins.getInstance()
 
@@ -18,66 +60,19 @@ def job = jenkins.getItem(jobName)
 // If job doesn't exist, create it
 if (job == null) {
     println "Creating pipeline job: ${jobName}"
-
+    
     // Create the job
     job = jenkins.createProject(WorkflowJob.class, jobName)
-
-    // Define Git SCM
-    def gitScm = new GitSCM(
-        [new UserRemoteConfig('git@gitlab.com:thorg/thorg-root.git', null, null, 'gitlab-ssh-key')],
-        [new BranchSpec('*/master-sandbox')],
-        false,
-        [],
-        null,
-        null,
-        []
-    )
-
-    // Set the pipeline definition using SCM
-    job.setDefinition(new CpsScmFlowDefinition(gitScm, 'Jenkinsfile'))
-
-    // Configure SCM polling with optimized settings
-    SCMTrigger trigger = new SCMTrigger('* * * * *')
-    trigger.setIgnorePostCommitHooks(false)
-
-    // Clear any existing triggers first
-    job.getTriggers().clear()
-
-    // Add the new trigger
-    job.addTrigger(trigger)
-
-    // Save the job
-    job.save()
-
+    
+    // Configure the job
+    configureJob(job)
+    
     println "Job '${jobName}' created successfully with optimized SCM polling configured."
 } else {
     println "Job '${jobName}' already exists. Updating configuration..."
-
-    // Update the existing job with proper SCM polling
-    def gitScm = new GitSCM(
-        [new UserRemoteConfig('git@gitlab.com:thorg/thorg-root.git', null, null, 'gitlab-ssh-key')],
-        [new BranchSpec('*/master-sandbox')],
-        false,
-        [],
-        null,
-        null,
-        []
-    )
-
-    // Set the pipeline definition using SCM
-    job.setDefinition(new CpsScmFlowDefinition(gitScm, 'Jenkinsfile'))
-
-    // Remove all existing triggers
-    job.getTriggers().clear()
-
-    // Configure SCM polling with optimized settings
-    SCMTrigger trigger = new SCMTrigger('* * * * *')
-    trigger.setIgnorePostCommitHooks(false)
-
-    // Add the new trigger
-    job.addTrigger(trigger)
-
-    job.save()
-
+    
+    // Update the existing job
+    configureJob(job)
+    
     println "Updated existing job '${jobName}' with optimized SCM polling configuration."
 }
